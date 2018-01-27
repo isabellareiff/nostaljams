1bef152c9e31b64b8e407c0eef97ecf7130d8aa6
Isabella Reiff <izzyreiff@gmail.com>
2018-01-26 17:22:44 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,372 @@
+////////// Youtube embed codes for disney songs ////////////
+var disney = [
+  "hZ1Rb9hC4JY", // 1 A whole new world
+  "afzmwAKUppU", // 2 Be our guest
+  "VNKuARjkWEg", // 3 Bibbidi-Bobbidi-Boo
+  "1_BtlAw4trg", // 4 Reflections
+  "9JDzlhW3XTM", // 5 I wanna be like you
+  "fhhLcszC-P4", // 6 Prince Ali
+  "Rl2qZvQg3Lc", // 7 A dream is wish your heart makes
+  "fbzEOQLOAWw", // 8 Bella Notte
+  "xB5ceAruYrI", // 9 Hakuna Matata
+  "8zLx_JtcQVI", // 10 Circle of Life
+  "0bGjlvukgHU", // 11 I just can't wait to be king
+  "j9MfuWSQoWc", // 12 I'll make a man out of you
+  "HvvZ1TEQRB8", // 13 Colors of the wind
+  "5tOXEPNvNB8", // 14 A girl worth fighting for
+  "4lCzBAQ2PLk", // 14 So this is love
+  "c-QVBpCAjXA", // 15 Poor unfortunate souls
+  "ZOe6Nsf-KGA", // 16 Work song (Cinderelly)
+  "2BZp2dxpwF8", // 17 Part of your world
+  "zgnHF2CwrPs", // 18 I can go the distance
+  "gLvK5nKbo6w", // 19 I won't say I'm in love
+  "jgA2xo0HYrE", // 20 Under the sea
+  "KCPRcAvB7MU", // 21 Once upon a dream
+  "kfULqbNgKCU", // 22 Hot dog hot dog hot diggity dog
+  "9Q13XS_R2mU", // 23 When you wish upon a star
+  "VqElJnsUvtE", // 24 Arabian nights
+  "aF4CWCXirZ8", // 25 Can you feel the love tonight
+  "RRq7lLawQB4", // 26 The Gospel Truth
+  "JI7cIYcnTRU", // 27 Everybody wants to be a cat
+  "KhJcyrkfjQg", // 28 He's a tramp
+  "AStiTWi2ABs", // 29 Siamese Cats
+  "xDUhINW3SPs", // 30 Tale as old as time
+  "fXTQWVqBDWY", // 31 Cruella de Ville
+  "3oEWA7UglB4", // 32 Savages
+  "4D26y7CQSHQ", // 33 Heaven's light/hellfire
+  "nPImqZo0D74", // 34 Love is an open door
+]
+////////// Youtube embed codes for theme songs used in an array ////////////
+var themes = [
+  "", // 1 
+  "", // 2 
+  "", // 3 
+  "", // 4 
+  "", // 5 
+  "", // 6 
+  "", // 7 
+  "", // 8 
+  "", // 9 
+  "", // 10 
+  // "", // 11 
+  // "", // 12 
+  // "", // 13 
+  // "", // 14 
+  // "", // 14 
+  // "", // 15 
+  // "", // 16 
+  // "", // 17 
+  // "", // 18 
+  // "", // 19 
+  // "", // 20 
+  // "", // 21
+  // "", // 22
+  // "", // 23
+  // "", // 24
+  // "", // 25
+  // "", // 26
+  // "", // 27
+  // "", // 28
+  // "", // 29
+  // "", // 30
+]
+///////// Youtube embed codes for Emo #tbts ///////
+var emo = [
+  // "", // 1 
+  // "", // 2 
+  // "", // 3 
+  // "", // 4 
+  // "", // 5 
+  // "", // 6 
+  // "", // 7 
+  // "", // 8 
+  // "", // 9 
+  // "", // 10 
+  // "", // 11 
+  // "", // 12 
+  // "", // 13 
+  // "", // 14 
+  // "", // 14 
+  // "", // 15 
+  // "", // 16 
+  // "", // 17 
+  // "", // 18 
+  // "", // 19 
+  // "", // 20 
+  // "", // 21
+  // "", // 22
+  // "", // 23
+  // "", // 24
+  // "", // 25
+  // "", // 26
+  // "", // 27
+  // "", // 28
+  // "", // 29
+  // "", // 30
+]
+///////// Youtube embed codes for R&B ///////
+var randb = [
+  // "", // 1 
+  // "", // 2 
+  // "", // 3 
+  // "", // 4 
+  // "", // 5 
+  // "", // 6 
+  // "", // 7 
+  // "", // 8 
+  // "", // 9 
+  // "", // 10 
+  // "", // 11 
+  // "", // 12 
+  // "", // 13 
+  // "", // 14 
+  // "", // 14 
+  // "", // 15 
+  // "", // 16 
+  // "", // 17 
+  // "", // 18 
+  // "", // 19 
+  // "", // 20 
+  // "", // 21
+  // "", // 22
+  // "", // 23
+  // "", // 24
+  // "", // 25
+  // "", // 26
+  // "", // 27
+  // "", // 28
+  // "", // 29
+  // "", // 30
+]
+///////// Youtube embed codes for Divas ///////
+var divas = [
+  // "", // 1 
+  // "", // 2 
+  // "", // 3 
+  // "", // 4 
+  // "", // 5 
+  // "", // 6 
+  // "", // 7 
+  // "", // 8 
+  // "", // 9 
+  // "", // 10 
+  // "", // 11 
+  // "", // 12 
+  // "", // 13 
+  // "", // 14 
+  // "", // 14 
+  // "", // 15 
+  // "", // 16 
+  // "", // 17 
+  // "", // 18 
+  // "", // 19 
+  // "", // 20 
+  // "", // 21
+  // "", // 22
+  // "", // 23
+  // "", // 24
+  // "", // 25
+  // "", // 26
+  // "", // 27
+  // "", // 28
+  // "", // 29
+  // "", // 30
+]
+///////// Youtube embed codes for Legends ///////
+var legends = [
+  // "", // 1 
+  // "", // 2 
+  // "", // 3 
+  // "", // 4 
+  // "", // 5 
+  // "", // 6 
+  // "", // 7 
+  // "", // 8 
+  // "", // 9 
+  // "", // 10 
+  // "", // 11 
+  // "", // 12 
+  // "", // 13 
+  // "", // 14 
+  // "", // 14 
+  // "", // 15 
+  // "", // 16 
+  // "", // 17 
+  // "", // 18 
+  // "", // 19 
+  // "", // 20 
+  // "", // 21
+  // "", // 22
+  // "", // 23
+  // "", // 24
+  // "", // 25
+  // "", // 26
+  // "", // 27
+  // "", // 28
+  // "", // 29
+  // "", // 30
+]
+////////// Variables for building the URLs ////////////
+var urlpre = 'https://www.youtube-nocookie.com/embed/';
+var urlpost = '?autoplay=1&controls=0&showinfo=0&rel=0&loop=1';
+////////// Functions happening onclick () ////////////
+function disneySing() {
+  document.getElementById('shuffle').src = urlpre + disney[Math.floor(Math.random() * disney.length)] + urlpost;
+}
+function themeSing() {
+  document.getElementById('shuffle').src = urlpre + themes[Math.floor(Math.random() * themes.length)] + urlpost;
+}
+function emoSing() {
+  document.getElementById('shuffle').src = urlpre + emo[Math.floor(Math.random() * emo.length)] + urlpost;
+}
+function randbSing() {
+  document.getElementById('shuffle').src = urlpre + randb[Math.floor(Math.random() * randb.length)] + urlpost;
+}
+function divaSing() {
+  document.getElementById('shuffle').src = urlpre + divas[Math.floor(Math.random() * divas.length)] + urlpost;
+}
+function legendSing() {
+  document.getElementById('shuffle').src = urlpre + legends[Math.floor(Math.random() * legends.length)] + urlpost;
+}
+////// End of Functions happening on click //////
+
+/*!
+ * Fairy Dust Cursor.js
+ * - 90's cursors collection
+ * -- https://github.com/tholman/90s-cursor-effects
+ * -- https://codepen.io/tholman/full/jWmZxZ/
+ */
+
+(function fairyDustCursor() {
+
+  var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var cursor = { x: width / 2, y: width / 2 };
+  var particles = [];
+
+  function init() {
+    bindEvents();
+    loop();
+  }
+
+  // Bind events that are needed
+  function bindEvents() {
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchstart', onTouchMove);
+
+    window.addEventListener('resize', onWindowResize);
+  }
+
+  function onWindowResize(e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+  }
+
+  function onTouchMove(e) {
+    if (e.touches.length > 0) {
+      for (var i = 0; i < e.touches.length; i++) {
+        addParticle(e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+      }
+    }
+  }
+
+  function onMouseMove(e) {
+    cursor.x = e.clientX;
+    cursor.y = e.clientY;
+
+    addParticle(cursor.x, cursor.y, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+  }
+
+  function addParticle(x, y, color) {
+    var particle = new Particle();
+    particle.init(x, y, color);
+    particles.push(particle);
+  }
+
+  function updateParticles() {
+
+    // Updated
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].update();
+    }
+
+    // Remove dead particles
+    for (var i = particles.length - 1; i >= 0; i--) {
+      if (particles[i].lifeSpan < 0) {
+        particles[i].die();
+        particles.splice(i, 1);
+      }
+    }
+
+  }
+
+  function loop() {
+    requestAnimationFrame(loop);
+    updateParticles();
+  }
+
+  /**
+   * Particles
+   */
+
+  function Particle() {
+
+    this.character = "*";
+    this.lifeSpan = 120; //ms
+    this.initialStyles = {
+      "position": "absolute",
+      "display": "block",
+      "pointerEvents": "none",
+      "z-index": "10000000",
+      "fontSize": "16px",
+      "will-change": "transform"
+    };
+
+    // Init, and set properties
+    this.init = function (x, y, color) {
+
+      this.velocity = {
+        x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+        y: 1
+      };
+
+      this.position = { x: x - 10, y: y - 20 };
+      this.initialStyles.color = color;
+
+      this.element = document.createElement('span');
+      this.element.innerHTML = this.character;
+      applyProperties(this.element, this.initialStyles);
+      this.update();
+
+      document.body.appendChild(this.element);
+    };
+
+    this.update = function () {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      this.lifeSpan--;
+
+      this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px, 0) scale(" + (this.lifeSpan / 120) + ")";
+    }
+
+    this.die = function () {
+      this.element.parentNode.removeChild(this.element);
+    }
+
+  }
+
+  /**
+   * Utils
+   */
+
+  // Applies css `properties` to an element.
+  function applyProperties(target, properties) {
+    for (var key in properties) {
+      target.style[key] = properties[key];
+    }
+  }
+
+  init();
+})();
